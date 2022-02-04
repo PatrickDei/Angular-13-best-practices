8c3c142109465a8943580b3fe895caf6a8a3ce70
Patrick Deisinger <patrick.deis@gmail.com>
2022-02-04 23:45:53 +0100

    Project skeleton

---
+++ Best-practices-showcase/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShowcaseComponent } from './component/showcase.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShowcaseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Angular-13-best-practices
+
+Best practices for your angular frontend project, covering data handling and passing between components
+
+###Handling templating with the Ivy engine
+[Templating showcase](src/app/component)
+
+###Handling data fetching from an API service
+[API service showcase](src/app/service)
+
+###Handling state
+[State management showcase](src/app/state)
